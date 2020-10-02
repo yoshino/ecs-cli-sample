a4e464fe963e4d9c7889a653906a308e280776f7
Taiki Yoshino <44nocycle@gmail.com>
2020-10-03 08:30:32 +0900

    override docker-compose's command by .env

--- bin/exec_docker_compose.sh
+++ bin/exec_docker_compose.sh
@@ -7,3 +7,8 @@ echo '----------------------------------'
 echo 'exec docker-compose override sample'
 docker-compose -f docker-compose.yml -f docker_compose_commands/docker-compose-command-sample.yml run app
 echo '----------------------------------'
+
+# REF: https://docs.docker.com/compose/environment-variables/#the-env-file
+echo 'exec docker-compose env sample'
+docker-compose -f docker-compose-use-env.yml run app
+echo '----------------------------------'
