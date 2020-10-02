e8c00ce6395790932d82f8468b37311735d13f97
Taiki Yoshino <44nocycle@gmail.com>
2020-10-03 08:45:30 +0900

    override docker-compose'command by args

--- bin/exec_docker_compose.sh
+++ bin/exec_docker_compose.sh
@@ -16,3 +16,9 @@ echo '----------------------------------'
 echo 'exec docker-compose override env sample'
 HOGE="override_env_hoge" FOO="override_env_foo" BAR="override_env_bar" docker-compose -f docker-compose-use-env.yml run app
 echo '----------------------------------'
+
+# you must specify args when exec this command.
+# ex) ./bin/exec_docker_compose.sh hoge_arg foo_arg bar_arg
+echo 'exec docker-compose override env sample by args'
+HOGE=$1 FOO=$2 BAR=$3 docker-compose -f docker-compose-use-env.yml run app
+echo '----------------------------------'
